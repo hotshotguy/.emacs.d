867ae6216b16fb96def58462ad2e4a9ea3dd7b24
Ivan Shamshurin <shamshurin.iv@yandex.ru>
2023-08-14 01:46:10 +0300

    move init tide to separate package

--- packages/typescript.el
+++ packages/typescript.el
@@ -1,22 +1,9 @@
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1)
-  (whitespace-mode 1))
-
 (leaf tide
   :ensure t)
 
 (leaf typescript-mode
   :ensure t
-  :after tree-sitter company
+  :after tree-sitter company ivansh-custom
   :config
   ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
   ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
@@ -29,9 +16,9 @@
   ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
   (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx))
   :init
-  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'typescript-mode-hook #'ivansh-setup-tide-mode)
   ;; if you use treesitter based typescript-ts-mode (emacs 29+)
-  (add-hook 'typescript-ts-mode-hook #'setup-tide-mode))
+  (add-hook 'typescript-ts-mode-hook #'ivansh-setup-tide-mode))
 
 
 (leaf apheleia
